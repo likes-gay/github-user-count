9f6e9f84c6f622b468aad2f9d2659f24215bc39b
Zoobdude <github@zoobdude.com>
2024-01-07 12:23:13 +0000

    start on backend

---
+++ backend.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, WebSocket
+import time
+
+app = FastAPI()
+
+def user_count():
+    #implement logic
+    
+    return(10, "John Doe", "https://avatars.githubusercontent.com/u/96008479") #placeholder values
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        user_count, last_user_name, last_user_pfp = user_count()
+        await websocket.send_json{
+            "user_count": user_count,
+            "last_user_name": last_user_name,
+            "last_user_pfp": last_user_pfp,
+            "time": time.time()    
+        }
